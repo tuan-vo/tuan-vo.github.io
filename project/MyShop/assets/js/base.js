37c1535b673bf7f77f8212a5f11c6a90f72ba875
Võ Văn Tuấn <64002460+tuanvo2908t@users.noreply.github.com>
2022-07-27 08:31:31 +0700

    Tuấn

--- project/MyShop/assets/js/base.js
+++ project/MyShop/assets/js/base.js
@@ -1,3 +1,6 @@
+var btnlogin1 = document.querySelector('.login'),
+    btnlogin2 = document.querySelector('.btn-login'),
+    formlogin = document.querySelector('.form-login');
 $(window).scroll(function(){
     if($('html,body').scrollTop()>150){
         $('header').addClass('bg-white');
@@ -5,3 +8,9 @@ $(window).scroll(function(){
         $('header').removeClass('bg-white');
     }
 })
+btnlogin1.onclick = function(){
+    formlogin.classList.toggle('openForm');
+}
+btnlogin2.onclick = function() {
+    formlogin.classList.remove('openForm');
+}
