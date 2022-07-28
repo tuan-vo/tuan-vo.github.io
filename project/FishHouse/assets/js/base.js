a3f33053a2ad8bace26c56cc0d20a09dce5c686e
Võ Văn Tuấn <64002460+tuanvo2908t@users.noreply.github.com>
2022-07-28 21:21:34 +0700

    Tuấn

--- project/FishHouse/assets/js/base.js
+++ project/FishHouse/assets/js/base.js
@@ -1,12 +1,30 @@
-$(window).scroll(function(){
-    if ($('html,body').scrollTop()>150) {
-        $('header').removeClass('w80');
-        $('header').addClass('bg-black');
-    } else {
-        $('header').addClass('w80');
-        $('header').removeClass('bg-black');
+var clickfull = document.querySelector('html,body');
+    header = document.querySelector('header'),
+    btnMenu = document.querySelector('.icon-menu'),
+    menu = document.querySelector('#menu_main'),
+    search = document.querySelector('.search'),
+    iconSearch = document.querySelector('.icon-search'),
+    ipSearch = document.querySelector('.js-ipSearch')
+    overlayfull = document.querySelector('.overlay-full');
+window.addEventListener('scroll',function(){
+    if(this.window.pageYOffset > 150){
+       header.classList.add('active')
+    }
+    else{
+       header.classList.remove('active')
     }
 })
-window.addEventListener('scroll',function(){
-    console.log(this.window.pageYOffset);
-})
+
+btnMenu.onclick = function(){
+    menu.classList.add('active');
+    overlayfull.classList.add('active');
+}
+overlayfull.onclick = function(){
+    menu.classList.remove('active');
+    overlayfull.classList.remove('active');
+}
+search.onclick = function(){
+    iconSearch.classList.remove('active');
+    ipSearch.classList.add('active');
+}
+
